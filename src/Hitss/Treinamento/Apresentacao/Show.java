ce3bca9376f6da242e0a6ad0f13ced4a725e36b1
marcio henrique <mhmuzy@embratel.com.br>
2020-12-23 12:22:51 -0300

    Criação do Programa do Detran para verificar o mês da Vistoria a partir do número final do carro [Treinamento]

---
+++ src/Hitss/Treinamento/Apresentacao/Show.java
@@ -0,0 +1,56 @@
+package Hitss.Treinamento.Apresentacao;
+
+import java.util.Scanner;
+
+public class Show {
+	public static void main(String[] args) {
+		try {
+		System.out.println("");
+		System.out.println("        Hitss Treinamento");
+		System.out.println("");
+		System.out.println("Prezado cliente, digite o número final de sua placa para a ver o mês da Vistoria:");
+		int num;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("");
+		num = teclado.nextInt();
+		System.out.println("");
+		switch (num) {
+		case 0:
+			System.out.println("A sua vistoria anual é para Janeiro de 2021.");
+			break;
+		case 1:
+			System.out.println("A sua vistoria anual é para Fevereiro de 2021.");
+			break;
+		case 2:
+			System.out.println("A sua vistoria anual é para Março de 2021.");
+			break;
+		case 3:
+			System.out.println("A sua vistoria anual é para Abril de 2021.");
+			break;
+		case 4:
+			System.out.println("A sua vistoria anual é para Maio de 2021.");
+			break;
+		case 5:
+			System.out.println("A sua vistoria anual é para Junho de 2021.");
+			break;
+		case 6:
+			System.out.println("A sua vistoria anual é para Setembro de 2021.");
+			break;
+		case 7:
+			System.out.println("A sua vistoria anual é para Outubro de 2021.");
+			break;
+		case 8:
+			System.out.println("A sua vistoria anual é para Novembro de 2021.");
+			break;
+		case 9:
+			System.out.println("A sua vistoria anual é para Dezembro de 2020, agora.");
+			break;
+		default:
+			System.out.println("Dígito Inválido.");
+			break;
+		}
+		} catch (Exception e) {
+			System.out.println("Erro de Processamento: " + e.getMessage());
+		}
+	}
+}
